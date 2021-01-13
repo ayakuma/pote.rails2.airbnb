7a85f1852f243b4d00aef15132c5839fda862dc2
ayaka <ayaka.71k@gmail.com>
2021-01-13 22:53:49 +0900

    1/13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email], pass: params[:pass]
+      # , img: params[:img], introduction: params[:introduction]
+    )
+    if @user.save
+      redirect_to("/users/index")
+    else
+      render("user/new")
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  get "/" => 'home#top'
+  get "/" => 'room#index'
+
+
+
+
+
+  get "users/index" => "users#index"
+  get "users/new" => "users#new"
+  post "users/create" => "users#create"
+  get "users/edit" => "users/edit"
+  
+
+
+
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
